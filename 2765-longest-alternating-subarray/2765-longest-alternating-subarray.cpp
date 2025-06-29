982420571ed5249c66a5bc5f9238f8a1c7a4e3ae
Saksham Jain <124597467+saks-hamjain@users.noreply.github.com>
2025-06-29 15:01:45 +0530

    Time: 0 ms (100%), Space: 70.5 MB (93.19%) - LeetHub

--- 2765-longest-alternating-subarray/2765-longest-alternating-subarray.cpp
+++ 2765-longest-alternating-subarray/2765-longest-alternating-subarray.cpp
@@ -4,17 +4,28 @@ public:
     {
       int n = arr.size();
       int maxi =-1;
-      for(int i =0;i<n;i++)
+      int start =-1;
+      for(int j=1;j<n;j++)
       {
-        for(int j =i+1;j<n;j++)
-        {
-            if(i%2 == j%2  && arr[j] == arr[i])
-                maxi = max(maxi,j-i+1);
-            else if(i%2 != j%2 && arr[j] == arr[i] +1)
-                maxi = max(maxi,j-i+1);
-            else break;
-
-        }
+            if(start ==-1 && arr[j] - arr[j-1] == 1)
+            {
+                 start = j-1;
+                 maxi = max(maxi,2);
+            }
+            else if(start != -1 && (j-start)%2 ==0 && arr[j] == arr[start])
+            maxi = max(maxi,j-start+1);
+            else if(start != -1 && (j-start) %2 ==1 && arr[j] == arr[start + 1])
+            maxi = max(maxi,j-start+1);
+            else 
+            {
+                if(arr[j] - arr[j-1] == 1)
+                {
+                    start = j-1;
+                 maxi = max(maxi,2);
+                }
+                else
+                start = -1;
+            }
       }
       return maxi;
     }
