1ede0d90f14c948c45558255e0aa6093d5ab269b
Saksham Jain <124597467+saks-hamjain@users.noreply.github.com>
2025-07-06 16:05:27 +0530

    Time: 0 ms (100%), Space: 14.7 MB (33.26%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) 
+    { 
+        vector<int> res;
+        for(int i=0;i<(1<<n);i++)
+        {
+            int val = i^(i>>1);
+            res.push_back(val);
+        }
+        return res;
+    }
+};
