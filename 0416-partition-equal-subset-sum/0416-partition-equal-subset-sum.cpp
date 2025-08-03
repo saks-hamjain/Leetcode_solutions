dffb25d105c4d59400b10ce713768339608eb325
Saksham Jain <124597467+saks-hamjain@users.noreply.github.com>
2025-08-03 22:24:12 +0530

    Time: 186 ms (14.84%), Space: 110.6 MB (32.58%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int isPossible(vector<int>& nums,int sum,int index,vector<vector<int>>& dp)
+    {
+       if(index == nums.size() && sum == 0) return 1;
+
+       if(index == nums.size() || sum < 0 ) return 0;
+
+       if(dp[index][sum] != -1) return dp[index][sum];
+
+       bool take = false;
+       if(nums[index] <= sum)
+        take = isPossible(nums,sum-nums[index],index+1,dp);
+       
+       bool not_take = isPossible(nums,sum,index+1,dp);
+       return dp[index][sum] = take || not_take;
+    }
+    bool canPartition(vector<int>& nums) 
+    {
+           long long total =0;
+           total = accumulate(nums.begin(),nums.end(),0);
+           if(total % 2 != 0) return false;
+           vector<vector<int>> dp(nums.size(),vector<int>((total/2)+1,-1));
+           bool answer = isPossible(nums,total/2,0,dp);
+           return answer; 
+    }
+};
