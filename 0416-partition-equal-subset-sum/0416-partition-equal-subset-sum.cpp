c218f406182daa24a57acc2b298c52f5c6437a3f
Saksham Jain <124597467+saks-hamjain@users.noreply.github.com>
2025-08-03 22:55:00 +0530

    Time: 84 ms (80.51%), Space: 13.3 MB (98.19%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -20,8 +20,16 @@ public:
            long long total =0;
            total = accumulate(nums.begin(),nums.end(),0);
            if(total % 2 != 0) return false;
-           vector<vector<int>> dp(nums.size(),vector<int>((total/2)+1,-1));
-           bool answer = isPossible(nums,total/2,0,dp);
-           return answer; 
+           total = total/2;
+        //    vector<vector<int>> dp(nums.size(),vector<int>((total/2)+1,-1));
+        //    bool answer = isPossible(nums,total/2,0,dp);
+        vector<bool> dp(total+1,false);
+        dp[0] = true;
+         for(auto num :nums)
+         {
+            for(int j = total;j>=num;j--)
+               dp[j]  = dp[j] || dp[j-num];
+         }
+           return dp[total]; 
     }
 };
