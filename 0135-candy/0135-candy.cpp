ec557929a8378372a87830d05acab7407d04b0e1
Saksham Jain <124597467+saks-hamjain@users.noreply.github.com>
2025-08-24 16:06:22 +0530

    Time: 0 ms (100%), Space: 22.8 MB (74.33%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -4,21 +4,15 @@ public:
         int n = ratings.size();
         if(n <= 1) return n;
         vector<int> tmp(n,1);
-        for(int i = 0;i<n;i++)
+        for(int i = 1;i<n;i++)
         {
-            int lc =0,left = i;
-            int rc =0,right=i;
-            while(left > 0 && ratings[left-1] < ratings[left])
-            {
-                lc++;
-                left--;
-            }
-            while(right < n-1 && ratings[right+1] < ratings[right])
-            {
-                right++;
-                rc++;
-            }
-            tmp[i] += max(lc,rc);
+            if(ratings[i] > ratings[i-1])
+             tmp[i] = max(tmp[i],tmp[i-1]+1);
+        }
+        for(int i= n-2;i>=0;i--)
+        {
+            if(ratings[i] > ratings[i+1])
+            tmp[i] = max(tmp[i],tmp[i+1]+1);
         }
 
     
