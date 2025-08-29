dc2e7e61a2f4169857241e22b015ee22a7f3e149
Saksham Jain <124597467+saks-hamjain@users.noreply.github.com>
2025-08-30 00:17:53 +0530

    Time: 1 ms (21.97%), Space: 12.5 MB (90.71%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -3,7 +3,7 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
 
         int n = triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n,INT_MAX));
+        // vector<vector<int>> dp(n,vector<int>(n,INT_MAX));
 
         // function<int(int,int)> dfs = [&](int i,int j){
         //   if(i == n-1) return triangle[i][j];
@@ -15,17 +15,22 @@ public:
         //   return dp[i][j] = min(same,right)+triangle[i][j];
         // };
         // int ans = dfs(0,0);
+        
+        vector<int> dp1(n,INT_MAX);
+        vector<int> dp2(n,INT_MAX);
+
         for(int i = n-1;i>= 0;i--)
-         dp[n-1][i] = triangle[n-1][i];
+         dp2[i] = triangle[n-1][i];
 
          for(int i = n-2;i>=0;i--)
          {
             for(int j = i;j>=0;j--)
             {
-                dp[i][j] = min(dp[i+1][j],dp[i+1][j+1]) + triangle[i][j];
+                dp1[j] = min(dp2[j],dp2[j+1]) + triangle[i][j];
             }
+            dp2 = dp1;
          }
-
-        return dp[0][0];
+        
+        return dp2[0];
     }
 };
