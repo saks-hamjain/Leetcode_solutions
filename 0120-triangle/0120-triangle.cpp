22b309ddac0ba15f84684cba272037f7a1c8caba
Saksham Jain <124597467+saks-hamjain@users.noreply.github.com>
2025-08-30 00:16:58 +0530

    Time: 1 ms (21.97%), Space: 13.4 MB (15.74%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -5,16 +5,27 @@ public:
         int n = triangle.size();
         vector<vector<int>> dp(n,vector<int>(n,INT_MAX));
 
-        function<int(int,int)> dfs = [&](int i,int j){
-          if(i == n-1) return triangle[i][j];
-          if(dp[i][j] != INT_MAX) return dp[i][j];
+        // function<int(int,int)> dfs = [&](int i,int j){
+        //   if(i == n-1) return triangle[i][j];
+        //   if(dp[i][j] != INT_MAX) return dp[i][j];
 
-          int same = dfs(i+1,j);
-          int right = dfs(i+1,j+1);
+        //   int same = dfs(i+1,j);
+        //   int right = dfs(i+1,j+1);
 
-          return dp[i][j] = min(same,right)+triangle[i][j];
-        };
-        int ans = dfs(0,0);
-        return ans;
+        //   return dp[i][j] = min(same,right)+triangle[i][j];
+        // };
+        // int ans = dfs(0,0);
+        for(int i = n-1;i>= 0;i--)
+         dp[n-1][i] = triangle[n-1][i];
+
+         for(int i = n-2;i>=0;i--)
+         {
+            for(int j = i;j>=0;j--)
+            {
+                dp[i][j] = min(dp[i+1][j],dp[i+1][j+1]) + triangle[i][j];
+            }
+         }
+
+        return dp[0][0];
     }
 };
